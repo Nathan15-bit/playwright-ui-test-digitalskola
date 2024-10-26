d032bd4ef870c0788dc5b440ea90a075441b75e4
Sangan Nathan <sangannathan2@gmail.com>
2024-10-26 18:05:30 +0700

    Final script for page-object until Checkout

--- tests/ui/page-object/cart-page.js
+++ tests/ui/page-object/cart-page.js
@@ -4,6 +4,13 @@ export class CartPage {
     constructor(page) {
         this.cartTitle = page.getByText('Your Cart');
         this.itemName = page.getByText('Sauce Labs Backpack');
+        this.checkoutButton = page.locator('[id="checkout"]');
+        this.firstNameTextbox = page.locator('[id="first-name"]');
+        this.lastNameTextbox = page.locator('[id="last-name"]');
+        this.postalCode = page.locator('[id="postal-code"]');
+        this.continueCheckout = page.locator('[id="continue"]');
+        this.finishTransaction = page.locator('[id="finish"]');
+        this.checkoutCompletedPage = page.getByText('Thank you for your order!');
     }
 
     async validateOnCartIcon() {
@@ -13,4 +20,39 @@ export class CartPage {
     async validateAddedImteDisplayed() {
         await expect(this.itemName).toBeVisible();
     }
+
+    async checkoutTheItem(){
+        await this.checkoutButton.click();
+    }
+
+    async inputFirstname(firstname) {
+        await this.firstNameTextbox.fill(firstname)
+    }
+
+    async inputLastname(lastname) {
+        await this.lastNameTextbox.fill(lastname)
+    }
+
+    async inputPostalcode(postalcode) {
+        await this.postalCode.fill(postalcode)
+    }
+
+    async checkoutInformation(firstname, lastname, postalcode){
+        await this.inputFirstname(firstname)
+        await this.inputLastname(lastname)
+        await this.inputPostalcode(postalcode)
+    }
+
+    async continueToCheckout(){
+        await this.continueCheckout.click();
+    }
+
+    async finishCheckout(){
+        await this.finishTransaction.click();
+    }
+
+    async validateSuccessfulCheckoutPage() {
+        await expect(this.checkoutCompletedPage).toBeVisible();
+    }
+
 }
--- tests/ui/saucedemo-po.spec.js
+++ tests/ui/saucedemo-po.spec.js
@@ -1,7 +1,7 @@
 const { afterEach } = require('node:test');
 const { test } = require('./base/base-test');
 
-test('TC 1 - Successfull login & add item to cart using STANDARD user', async ({ loginPage, dashboardPage, cartPage }) => {
+test('TC 1 - Successfull login until Checkout item from Cart using STANDARD user', async ({ loginPage, dashboardPage, cartPage }) => {
 
     await loginPage.login(process.env.STANDARD_USER, process.env.PASSWORD)
     await dashboardPage.validateOnPage()
@@ -10,10 +10,15 @@ test('TC 1 - Successfull login & add item to cart using STANDARD user', async ({
     await dashboardPage.clickCartIcon()
     await cartPage.validateOnCartIcon()
     await cartPage.validateAddedImteDisplayed()
+    await cartPage.checkoutTheItem()
+    await cartPage.checkoutInformation(process.env.FIRSTNAME, process.env.LASTNAME, process.env.POSTALCODE)
+    await cartPage.continueToCheckout()
+    await cartPage.finishCheckout()
+    await cartPage.validateSuccessfulCheckoutPage()
 });
 
 
-test('TC 2 - Successfull login & add item to cart using VISUAL user', async ({ loginPage, dashboardPage, cartPage }) => {
+test('TC 2 - Successfull login until Checkout item from Cart using VISUAL user', async ({ loginPage, dashboardPage, cartPage }) => {
    
     await loginPage.login(process.env.VISUAL_USER, process.env.PASSWORD)
     await dashboardPage.validateOnPage()
@@ -22,6 +27,11 @@ test('TC 2 - Successfull login & add item to cart using VISUAL user', async ({ l
     await dashboardPage.clickCartIcon()
     await cartPage.validateOnCartIcon()
     await cartPage.validateAddedImteDisplayed()
+    await cartPage.checkoutTheItem()
+    await cartPage.checkoutInformation(process.env.FIRSTNAME, process.env.LASTNAME, process.env.POSTALCODE)
+    await cartPage.continueToCheckout()
+    await cartPage.finishCheckout()
+    await cartPage.validateSuccessfulCheckoutPage()
 });
 
 // test.beforeAll(async () => {
